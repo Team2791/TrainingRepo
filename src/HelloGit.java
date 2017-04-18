47bce61df8c6689740deb45d7758ce188c0cf8ef
Shaker Robotics <FRC2791@Benjamins-MacBook-Air.local>
2017-04-18 11:26:31 -0400

    noah is a dick to me

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,6 +2,7 @@
 public class HelloGit {
 	public static void Main(String args[]){
 		System.out.println("Hello Git");
+		System.out.print(" noah is a dick");
 	}
 }
-//AYYY ITS YA BOI
+//AYYY ITS YA BOI AHHHHHHHHHHHHHHHHHH
